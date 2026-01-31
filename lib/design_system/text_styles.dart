19a6e7795a21c6b5ce7a6b7d003a955e65983145
ObsidianXVI <siddharth.chitikela@gmail.com>
2026-01-31 19:23:07 +0800

    added font assets

--- lib/design_system/text_styles.dart
+++ lib/design_system/text_styles.dart
@@ -2,9 +2,13 @@ part of axis_dashboard;
 
 final TextStyle appBarTitle = TextStyle(
   color: AxisColors.white50,
+  fontFamily: 'Urbanist',
+  fontWeight: FontWeight.w500,
+  fontSize: 26,
 );
 
 final TextStyle buttonLabel = TextStyle(
   color: AxisColors.blackPurple20,
   fontSize: 18,
+  fontFamily: 'Urbanist',
 );
